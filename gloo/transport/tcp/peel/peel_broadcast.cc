99142a3001e8e942f52d9e5fa2245fd6dc106a49
root <root@xelar.ad1.asleague.org>
2026-04-14 18:30:14 +0800

    Possible issue: barrier

--- gloo/transport/tcp/peel/peel_broadcast.cc
+++ gloo/transport/tcp/peel/peel_broadcast.cc
@@ -10,7 +10,12 @@ namespace tcp {
 namespace peel {
 
 PeelBroadcast::PeelBroadcast(PeelTransport* transport)
-    : transport_(transport) {}
+    : transport_(transport) {
+    
+    std::cerr << "peel[" << rank_ << "]: broadcast() called, root=" << root 
+            << ", size=" << size << "\n";
+    
+    }
 
 bool PeelBroadcast::run(int root, void* data, size_t size) {
     if (!transport_ || !transport_->isReady()) {
