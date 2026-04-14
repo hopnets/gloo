1e51a597c74ce377d452939818f126178666d1dd
root <root@xelar.ad1.asleague.org>
2026-04-14 18:36:30 +0800

    add debug info

--- gloo/transport/tcp/peel/peel_broadcast.cc
+++ gloo/transport/tcp/peel/peel_broadcast.cc
@@ -10,18 +10,19 @@ namespace tcp {
 namespace peel {
 
 PeelBroadcast::PeelBroadcast(PeelTransport* transport)
-    : transport_(transport) {
-    
-    std::cerr << "peel[" << rank_ << "]: broadcast() called, root=" << root 
-            << ", size=" << size << "\n";
-    
-    }
+    : transport_(transport) {}
 
 bool PeelBroadcast::run(int root, void* data, size_t size) {
+    
+
+
+
     if (!transport_ || !transport_->isReady()) {
         std::cerr << "peel_broadcast: transport not ready\n";
         return false;
     }
+
+
 
     return transport_->broadcast(root, data, size);
 }
